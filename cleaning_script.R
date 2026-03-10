18fbaa9dac000b258b306471c7ed791bfa9151d0
arnavk377 <arnavk1321@gmail.com>
2026-03-10 15:09:43 -0700

    forgor to save the changes for cleaning

--- cleaning_script.R
+++ cleaning_script.R
@@ -2,16 +2,25 @@ library(tidyverse)
 
 files <- list.files(path = "./data/", pattern = "*.csv", full.names = TRUE)
 
-raw_data <- map_df(files, ~read_csv(.x, show_col_types = FALSE) %>% 
-                     mutate(year = str_extract(.x, "\\d{4}")), # Extracts the 4-digit year from filename
-                   .id = "source")
+raw_data <- map_df(files, ~read_csv(.x, 
+                                    show_col_types = FALSE,
+                                    col_select = -Rk) %>%  # This drops 'Rk' immediately
+                     mutate(year = str_extract(.x, "\\d{4}")))
 
-clean_data <- data %>% 
+clean_data <- raw_data %>% 
   filter(Pos == 'QB') %>% # filter to keep only quarterbacks
   filter(GS > 3) %>% # filter out players who have played only a few games
+  filter(Att > 50) %>% # filter out players who did not attempt many passes
+  group_by(Player, year) %>% 
+  slice_max(order_by = GS, n = 1, with_ties = FALSE) %>% 
+  ungroup() %>%
   separate(QBrec, into = c("Wins"), sep = "-", extra = "drop", convert = TRUE) %>% # extract number of wins
   select(
-    c("year", "Player", "Age", "GS", "Wins", "Cmp", "Att", "Yds...12", "TD", "Int", "Succ%", "Rate", "QBR", "Sk")
+    c("year", "Player", "Age", "GS", "Wins", "Cmp", "Att", "Yds...11", "TD", "Int", "Succ%", "Rate", "QBR", "Sk")
   ) %>% 
-  rename(Yards = Yds...12, Completions = Cmp, Attempts = Att, Interceptions = Int, Sacks = Sk) # rename column for cleanliness
+  rename(Yards = Yds...11, Completions = Cmp, Attempts = Att, Interceptions = Int, Sacks = Sk) # rename column for cleanliness
 
+# write clean data to a csv file for usage in other files
+write_csv(clean_data, "./data/cleaned_data.csv")
+
+# read_csv("./data/cleaned_data.csv")
